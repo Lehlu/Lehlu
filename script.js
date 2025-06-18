0ef47480a58e564fc086f6040d153de016e96f5c
Lehlu <lehlu.com@gmail.com>
2025-06-17 22:36:09 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,12 +1,1 @@
-
-const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-const container = document.getElementById("letters");
-for (let i = 0; i < 25; i++) {
-  const span = document.createElement("span");
-  span.className = "letter";
-  span.textContent = letters[Math.floor(Math.random() * letters.length)];
-  span.style.left = Math.random() * 100 + "vw";
-  span.style.top = Math.random() * 100 + "vh";
-  span.style.animationDuration = (5 + Math.random() * 10) + "s";
-  container.appendChild(span);
-}
+console.log('Lehlu site loaded');
